625dcae25f0a5d14b3b7b7e2c393612641d4b2c0
Harsh <harshchoudhary06026@gmail.com>
2026-02-13 14:01:53 +0530

    Initial Spring Boot setup

---
+++ team-project/src/main/java/com/example/team/project/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.team.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
